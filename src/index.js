db7fbcf80f1343d6fd01179dd21dbf0f5bab8763
Xussof <xussof@gmail.com>
2021-08-26 19:16:37 +0200

    Push

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 const serverless = require('serverless-http');
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-app.post("/api", (req, res, next) => {
- res.json(["Tony","Gol"]);
-});
-
-app.listen(3000, () => {
- console.log("Server running on port 3000");
-});
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
 module.exports.handler = serverless(app);
