61f18f338bff1ce4ee1910896e952ba98dff47df
Xussof <xussof@gmail.com>
2021-08-26 19:22:25 +0200

    Push

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
 
-app.get('/api', function (req, res) {
-  res.send('Hello World!')
+app.get('/', function (req, res) {
+  res.json(["Tony","Gol"]);
 })
 
 module.exports.handler = serverless(app);
